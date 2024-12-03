1a346dbcbad49e641ccb3c80e6cbc39ab61fb840
Quý Trinh <nguyentrinhquy1411@gmail.com>
2024-12-03 13:58:03 +0700

    fix deploy

---
+++ js/config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                poppins: ['Poppins', 'sans-serif'],
+                roboto: ['Roboto', 'sans-serif'],
+                inter: ['Inter', 'sans-serif'],
+            },
+            colors: {
+                blackText: '#1f1f1f',
+                yellowText: '#f6af03',
+                blackBlackGround: '#011c2b',
+                footerBG: '#011c2a',
+                pageBG: '#FBFBFB',
+                subBG: '#011621',
+                subBtn: '#000e15',
+                grayText: '#717171',
+            },
+        }
+    }
+}
